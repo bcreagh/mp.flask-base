3d041b860fbcb143bb3ae6ff421c30b708bf8715
Brian Creagh <briancreagh95@gmail.com>
2019-04-03 20:25:16 +0100

    Add get_action() method to ActionService

--- src/services/action_service.py
+++ src/services/action_service.py
@@ -14,5 +14,16 @@ class ActionService:
     def get_all_actions():
         return ActionService._actions
 
+    @staticmethod
+    def get_action(route):
+        result = None
+        for action in ActionService._actions:
+            if action['route'] == route:
+                result = action
+        if result is not None:
+            return result
+        else:
+            raise ValueError('An action could not be found for the route {}'.format(route))
+
 
 ActionService._initialise()
